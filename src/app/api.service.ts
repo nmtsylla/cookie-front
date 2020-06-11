49fcff969838f60fb0a81fefe1443c70181e8281
bsylla <babacar.sylla@altran.com>
2020-06-11 14:04:28 +0200

    Query websites in main component

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,58 +1,60 @@
+import { environment } from '../environments/environment';
 import { Injectable } from '@angular/core';
 import { Observable, of, throwError } from 'rxjs';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
 import { catchError, tap, map } from 'rxjs/operators';
 import { Website } from './models/website';
 
 const httpOptions = {
-  headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
-const apiUrl = '/api/v1';
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
+const apiUrl = environment.apiUrl + '/websites';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor(private http: HttpClient) { }
+  constructor (private http: HttpClient) {}
 
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-      console.error(error);
-      return of(result as T);
-    };
+      console.error(error)
+      return of(result as T)
+    }
   }
 
-  getWebsites(): Observable<Website[]> {
-    return this.http.get<Website[]>(`${apiUrl}`)
-      .pipe(
-        tap(websites => console.log('fetched websites')),
-        catchError(this.handleError('getWebsites', []))
-      );
+  getWebsites (): Observable<Website[]> {
+    return this.http.get<Website[]>(`${apiUrl}`).pipe(
+      tap(websites => console.log('fetched websites')),
+      catchError(this.handleError('getWebsites', []))
+    )
   }
 
-  getWebsiteById(id: number): Observable<Website> {
-    const url = `${apiUrl}/${id}`;
+  getWebsiteById (id: number): Observable<Website> {
+    const url = `${apiUrl}/${id}`
     return this.http.get<Website>(url).pipe(
       tap(_ => console.log(`fetched Website id=${id}`)),
       catchError(this.handleError<Website>(`getWebsiteById id=${id}`))
-    );
+    )
   }
 
-  addWebsite(website: Website): Observable<Website> {
+  addWebsite (website: Website): Observable<Website> {
     return this.http.post<Website>(apiUrl, website, httpOptions).pipe(
       tap((c: Website) => console.log(`added Website id=${c.id}`)),
       catchError(this.handleError<Website>('addWebsite'))
-    );
+    )
   }
 
-  deleteWebsite(id: number): Observable<Website> {
-    const url = `${apiUrl}/${id}`;
+  deleteWebsite (id: number): Observable<Website> {
+    const url = `${apiUrl}/${id}`
     return this.http.delete<Website>(url, httpOptions).pipe(
       tap(_ => console.log(`deleted Website id=${id}`)),
       catchError(this.handleError<Website>('deleteWebsite'))
-    );
+    )
   }
-
-
 }
--- src/app/websites/websites.component.ts
+++ src/app/websites/websites.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Website } from '../models/website';
 
 @Component({
   selector: 'app-websites',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebsitesComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['url', 'weeklyScanDay', 'scanSchedule', 'addedDate', 'lastScanned', 'customerId'];
+  data: Website[] = [];
+  isLoadingResults = true;
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.api.getWebsites()
+    .subscribe((res: any) => {
+      this.data = res;
+      console.log(this.data);
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
   }
 
 }
