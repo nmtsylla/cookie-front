ea419d2440dc3d7d4d5687f7bc6f3ed5a154e59a
bsylla <babacar.sylla@altran.com>
2020-06-11 13:18:54 +0200

    Register component in the router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,37 @@
+import { WebsitesComponent } from './websites/websites.component';
+import { WebsitesDetailComponent } from './websites-detail/websites-detail.component';
+import { WebsiteEditComponent } from './website-edit/website-edit.component';
+import { WebsiteAddComponent } from './website-add/website-add.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'websites',
+    component: WebsitesComponent,
+    data: { title: 'List of websites' }
+  },
+  {
+    path: 'website-details/:id',
+    component: WebsitesDetailComponent,
+    data: { title: 'Website Details' }
+  },
+  {
+    path: 'add-website',
+    component: WebsiteAddComponent,
+    data: { title: 'Add Website' }
+  },
+  {
+    path: 'edit-website/:id',
+    component: WebsiteEditComponent,
+    data: { title: 'Edit website' }
+  },
+  { path: '',
+    redirectTo: '/websites',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
